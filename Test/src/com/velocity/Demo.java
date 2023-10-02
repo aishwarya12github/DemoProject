8834fc6a6ff605ae004f4419c0df826ac0327ed3
aishwarya <kindreaishwarya12@gmail.com>
2023-10-02 21:55:22 +0530

    list is added

---
+++ Test/src/com/velocity/Demo.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		List l=new ArrayList();
+		l.add(10);
+		l.add(20);
+		System.out.println(l);
+
+	}
+
+}
